71588dd978e5a8d08a761d191768208aecac4357
sergeybwk <sergeybwk@gmail.com>
2024-08-18 04:56:09 +0300

    Added auto_route, implemented adding a new dish in app (currently w/o firebase)

---
+++ lib/src/utils/router.gr.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'router.dart';
+
+/// generated route for
+/// [MainScreen]
+class MainRoute extends PageRouteInfo<void> {
+  const MainRoute({List<PageRouteInfo>? children})
+      : super(
+          MainRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'MainRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return BlocProvider<MainScreenBloc>(
+          create: (_) => MainScreenBloc(mainScreenRepository: MainScreenRepositoryFirebase())..add(InitMainScreen()),
+          child: MainScreen());
+    },
+  );
+}
